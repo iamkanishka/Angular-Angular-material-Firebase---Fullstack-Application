8099682ff3231c54f434b47b353e7a57bcf6030d
iamkanishka <kanishkabc123@gmail.com>
2022-02-07 23:41:45 +0530

    added crud oprtation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,140 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore,ɵAngularFireSchedulers} from '@angular/fire';
+import { FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {  Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { group } from '@angular/animations';
+
+
+export interface details{
+  
+  email:string;
+  password:string;
+  phone:number;
+  age:number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kanishkanaik';
+export class AppComponent  {
+  title = 'CRUD Operation';
+   myform: FormGroup;
+
+constructor(private afs:AngularFirestore,private fb:FormBuilder){}
+
+private detailcollection:AngularFirestoreCollection<details>;
+details:Observable<details[]>;
+showl:boolean=true;
+showu:boolean=false;
+detail:any;
+
+ngOnInit(): void {
+  
+ 
+ this.detailcollection=this.afs.collection<details>('details');
+ this.details=this.detailcollection.valueChanges();
+ 
+  this.myform=this.fb.group({
+    detailno:['',[
+      Validators.required
+    ]],
+    email:['',[
+      Validators.email,
+      Validators.required
+    ]],
+  password:['',[
+    Validators.required,
+   Validators.maxLength(10),
+   Validators.minLength(5)
+  ]],
+  phone:['',[
+    Validators.required,
+    Validators.maxLength(10),
+    Validators.minLength(10)
+  ]],
+  age:['',[
+    Validators.max(50),
+    Validators.maxLength(2),
+    Validators.min(18),
+    Validators.required
+  ]],
+ 
+  });
 }
+
+get detailno(){
+  return this.myform.get('detailno');
+}
+get email(){
+  return this.myform.get('email');
+}  
+get password(){
+  return this.myform.get('password');
+}  
+get phone(){
+  return this.myform.get('phone');
+}  
+get age(){
+  return this.myform.get('age');
+}  
+get agree(){
+  return this.myform.get('agree');
+}
+
+async submitHandler() {
+  const formValue = this.myform.value;
+try {
+    await this.afs.collection('details').add(formValue);
+    console.log(formValue);
+    this.myform.reset();
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+
+
+edorddet:any;
+
+shows:boolean=true;
+
+preloadData(Event:any) {
+ this.shows=false;
+  
+  this.showu=true;
+  this.edorddet=Event;
+  this.afs.collection('details').doc(this.edorddet.id).valueChanges().pipe(
+    tap(data => {
+      this.myform.patchValue(data)
+    })
+  )
+  .subscribe()
+}
+
+async updateData(){
+  
+  this.shows=false;
+  this.showu=false;
+  const formValue = this.myform.value;
+  try {
+    await this.afs.collection('details').doc(this.edorddet.id).update(formValue);
+    this.myform.reset();
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+
+
+}
+
+
+
+
+
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import { DetailstableComponent } from './detailstable/detailstable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailstableComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    MatTableModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
